a613f5e2afc03f303a690c27d2f18cc33e8010a9
Kirellos-Safwat <kero.safwat763@gmail.com>
2024-10-13 22:19:28 +0300

    added tree object

--- scene.cpp
+++ scene.cpp
@@ -1,7 +1,3 @@
-//cr. code: Sumanta Guha.
-////////////////////////
-
-//importing the header files
 #include <GL/glew.h>
 #include <GL/freeglut.h> 
 #include <math.h>
@@ -17,13 +13,13 @@ void drawCircle(float cx, float cy, float r, int num_segments) {
 	glEnd();
 }
 
-void drawGrass() {
+void drawGrass(float x, float y) {
 	glColor3f(0.0, 1.0, 0.0); // Green color
 	glBegin(GL_POLYGON);
 	glVertex3f(0.0, 0.0, 0.0);
-	glVertex3f(300.0, 0.0, 0.0);
-	glVertex3f(300.0, 40.0, 0.0); // Height of the grass
-	glVertex3f(0.0, 40.0, 0.0);
+	glVertex3f(x, 0.0, 0.0);
+	glVertex3f(x, y, 0.0); // Height of the grass
+	glVertex3f(0.0, y, 0.0);
 	glEnd();
 }
 
@@ -83,7 +79,7 @@ void drawHouse() {
 	glVertex3f(25.0, 65.0, 0.0);//top left
 	glEnd();
 
-	glColor3f(0.0, 0.0, 0.0); 
+	glColor3f(0.0, 0.0, 0.0);
 	drawCircle(80, 90, 5, 100);
 
 	glBegin(GL_POLYGON); // Door
@@ -95,104 +91,170 @@ void drawHouse() {
 	drawCircle(80, 50, 10, 100);
 }
 
+void drawFruit(float x, float y) {
+	glColor3f(1.0, 0.0, 0.0); // Red color for the fruit
+	drawCircle(x, y, 3, 100); // Draw the fruit as a small circle
+}
+
+void drawGrassBlade(float x, float y) {
+	glColor3f(0.0, 0.65, 0.0); // Green color for the grass
+	glBegin(GL_TRIANGLES);
+	glVertex2f(x, y + 10); // Base of the grass
+	glVertex2f(x - 2, y); // Left tip
+	glVertex2f(x + 2, y); // Right tip
+	glEnd();
+}
+
+
+void drawTree(float x, float y) {
+	// Draw trunk
+	glColor3f(0.545, 0.271, 0.075); // Brown color for the trunk
+	glBegin(GL_POLYGON);
+	glVertex2f(x - 10, y);    // Bottom left
+	glVertex2f(x + 10, y);    // Bottom right
+	glVertex2f(x + 5, y + 50); // Top right
+	glVertex2f(x - 5, y + 50); // Top left
+	glEnd();
+
+	// Draw foliage
+	glColor3f(0.0, 0.65, 0.0); // Green color for the leaves
+	drawCircle(x, y + 75, 30, 100); // Foliage circle
+	drawCircle(x + 25, y + 75, 15, 100);
+	drawCircle(x - 25, y + 75, 15, 100);
+	drawCircle(x, y + 100, 15, 100);
+	drawCircle(x, y + 55, 15, 100);
+	drawCircle(x + 20, y + 90, 15, 100);
+	drawCircle(x + 20, y + 60, 15, 100);
+	drawCircle(x - 20, y + 90, 15, 100);
+	drawCircle(x - 20, y + 60, 15, 100);
+	// Draw fruits
+	drawFruit(x - 22.5, y + 55);
+	drawFruit(x + 22.5, y + 55);
+	drawFruit(x, y + 50);
+	drawFruit(x - 30, y + 75);
+	drawFruit(x + 30, y + 75);
+	drawFruit(x, y + 105);
+	drawFruit(x - 22.5, y + 92.5);
+	drawFruit(x + 22.5, y + 92.5);
+
+	drawFruit(x - 10, y + 70);
+	drawFruit(x + 10, y + 70);
+	drawFruit(x, y + 85);
+
+	drawGrassBlade(x - 9, y);
+	drawGrassBlade(x + 9, y);
+	drawGrassBlade(x - 6, y - 0.5);
+	drawGrassBlade(x + 6, y - 0.5);
+	drawGrassBlade(x - 3, y - 1);
+	drawGrassBlade(x + 3, y - 1);
+	drawGrassBlade(x, y - 1.5);
+}
+
+void drawCloud(float x, float y) {
+	// Draw clouds
+	glColor3f(1.0, 1.0, 1.0); // White color
+	drawCircle(x - 25, y, 20, 100); // left
+	drawCircle(x + 25, y, 20, 100); // right
+	drawCircle(x, y, 30, 100); // center
+	//drawCircle(x, y + 10, 30, 100); // top
+}
+
+
 // windmill stand
-void windmillStand()
+void windmill(float x, float y)
 {
 	glBegin(GL_QUADS);
-	glColor3f(0.69, 0.43, 0.314);
-	glVertex2i(240, 40);
-	glVertex2i(260, 40);
-	glVertex2i(255, 120);
-	glVertex2i(245, 120);
+	glColor3f(0.7, 0.7, 0.075);
+	glVertex2i(x - 20, y - 30);
+	glVertex2i(x + 20, y - 30);
+	glVertex2i(x + 15, y + 30);
+	glVertex2i(x - 15, y + 30);
 	glEnd();
+
+	glColor3f(0.8, 0.0, 0.0); // 
+	glBegin(GL_TRIANGLES);
+	glVertex2f(x, y + 60); // 
+	glVertex2f(x - 30, y + 30); // 
+	glVertex2f(x + 30, y + 30);
+
+	// wings
+	glColor3f(0.4, 0.176, 0.070);
+	glVertex2f(x, y + 45);
+	glVertex2f(x + 32, y + 22.5);
+	glVertex2f(x + 20, y + 12.5);
+
+	glVertex2f(x, y + 45);
+	glVertex2f(x - 32, y + 22.5);
+	glVertex2f(x - 20, y + 12.5);
+
+	glVertex2f(x, y + 45);
+	glVertex2f(x + 32, y + 67.5);
+	glVertex2f(x + 20, y + 77.5);
+
+	glVertex2f(x, y + 45);
+	glVertex2f(x - 32, y + 67.5);
+	glVertex2f(x - 20, y + 77.5);
+	glEnd();
+
+	drawCircle(x, y + 45, 4, 100);
+
+	glBegin(GL_POLYGON); // Door
+	glVertex2f(x - 7.5, y - 30);
+	glVertex2f(x + 7.5, y - 30);
+	glVertex2f(x + 7.5, y - 10);
+	glVertex2f(x - 7.5, y - 10);
+	glEnd();
+	drawCircle(x, y + 15, 7.5, 100);
+
 }
 
-// windmill wings
-void windmillWings()
-{
-	//1st wing
-	glBegin(GL_QUADS);
-	glColor3f(0.4, 0.176, 0.070);
-	glVertex2i(272, 90);
-	glVertex2i(269, 104);
-	glVertex2i(229, 150);
-	glVertex2i(232, 136);
-
-	//2nd wing	
-	glColor3f(0.4, 0.176, 0.070);
-	glVertex2i(224, 90);
-	glVertex2i(274, 136);
-	glVertex2i(279, 150);
-	glVertex2i(229, 104);
-	glEnd();
-}
-
-
-// Drawing routine.
-void drawScene(void)
-{
+void drawScene(void) {
 	// Clearing the buffer and setting the drawing color
 	glClear(GL_COLOR_BUFFER_BIT);
-	
-	
+
 	// Draw the grass
-	drawGrass();
+	drawGrass(600, 40);
+
 
 	// Draw the house
 	drawHouse();
 
-	// Draw windmill stand
-	windmillStand();
+	// Draw the tree
+	drawTree(400, 20); // Position the tree
+	drawTree(500, 20); // Position the tree
 
-	// Draw windmill wings
-	windmillWings();
+	drawCloud(100, 250);
+	drawCloud(300, 280);
+	drawCloud(500, 265);
 
-
-	// Draw clouds
-	glColor3f(1.0, 1.0, 1.0); // White color
-	drawCircle(140, 250, 30, 100); // Cloud 1
-	drawCircle(185, 250, 30, 100); // Cloud 2
-	drawCircle(165, 240, 30, 100); // Cloud 3
-	drawCircle(165, 260, 30, 100); // Cloud 4
-
+	// Draw windmill 
+	windmill(280, 60);
 
 	// Execute the drawing
 	glFlush();
 }
 
-
 // Initialization routine.
-void setup(void)
-{
-	//the clearing color of the opengl window (background)
+void setup(void) {
+	// the clearing color of the opengl window (background)
 	glClearColor(0.53, 0.81, 0.92, 0.0); // Background color
 }
 
 // OpenGL window reshape routine.
-void resize(int w, int h)
-{
+void resize(int w, int h) {
 	// drawing the entire window
 	glViewport(0, 0, w, h);
-
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-
-	//glOrtho(left, right, bottom, top, near, far)
-	//Sets up a viewing box span along the x-axis is from left to right, along the y-axis from bottom to top, 
-	// and along the z-axis from ?far to ?near.
-	glOrtho(0.0, 300.0, 0.0, 300.0, -1.0, 1.0);
-
+	glOrtho(0.0, 600.0, 0.0, 400.0, -1.0, 1.0);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
 
 // Keyboard input processing routine. x & y are location of the mouse
-void keyInput(unsigned char key, int x, int y)
-{
-	switch (key)
-	{
-		// ascii of escape key
-	case 27:
+void keyInput(unsigned char key, int x, int y) {
+	switch (key) {
+	case 27: // ascii of escape key
 		exit(0);
 		break;
 	default:
@@ -201,40 +263,22 @@ void keyInput(unsigned char key, int x, int y)
 }
 
 // Main routine.
-int main(int argc, char** argv)
-{
-
-	glutInit(&argc, argv);	// initializes the FreeGLUT library.
-
-	// create context, set its version and set backward compatibility. 
-	// context is the interface between an instance of OpenGL and the rest of the system
+int main(int argc, char** argv) {
+	glutInit(&argc, argv); // initializes the FreeGLUT library.
 	glutInitContextVersion(3, 3);
 	glutInitContextProfile(GLUT_COMPATIBILITY_PROFILE);
-
-	// wanting an OpenGL context to support a single-buffered frame, each pixel having red, green, blue and alpha values.
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-
-	// set the initial size of the OpenGL window and the location of its top left corner on the computer screen
-	glutInitWindowSize(400, 400);
+	glutInitWindowSize(600, 400);
 	glutInitWindowPosition(200, 200);
-
-	// creates the OpenGL context and its associated window with the specified string parameter as title.
 	glutCreateWindow("nature.cpp");
 
-	// callback routines – when the OpenGL window is to be drawn, when it is resized, 
-	// and when keyboard input is received, respectively
 	glutDisplayFunc(drawScene);
 	glutReshapeFunc(resize);
 	glutKeyboardFunc(keyInput);
 
-	// initializes GLEW (the OpenGL Extension Wrangler Library) which handles the loading of OpenGL extensions, 
-	// with the switch set so that extensions implemented even in pre-release drivers are exposed.
 	glewExperimental = GL_TRUE;
 	glewInit();
 
 	setup();
-
-	// begins the event-processing loop, calling registered callback routines as needed
 	glutMainLoop();
-
 }
